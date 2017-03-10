e490e73828fb804ed98baa1dd970a3eefe63c2bf
Fateh <fateh288@gmail.com>
2017-03-10 20:07:44 +0530

    Loss function working

--- trial.cpp
+++ trial.cpp
@@ -23,22 +23,17 @@ double sum(double arr[],int n){
 	}
 	return sum;
 }
-void copy_matrix(double **arr1,int m,int n,double ***arr3){
+void copy_matrix(double **arr1,int m,int n,double **arr2){
 	int i,j;
-	double **arr2=new double*[m];
-	for(i=0;i<m;i++){
-		arr2[i]=new double[n];
-	}
 	for(i=0;i<m;i++){
 		for(j=0;j<n;j++){
 			arr2[i][j]=arr1[i][j];
 		}
 	}
-	arr3=&arr2;
 }
 
-void mat_row_wise_sum(double **arr1,int m,int n,double **sm){
-	double *sum=new double[m];
+void mat_row_wise_sum(double **arr1,int m,int n,double *sum){
+	//double *sum=new double[m];
 	int i,j;
 	for(int i=0;i<m;i++){
 		sum[i]=0;
@@ -46,29 +41,20 @@ void mat_row_wise_sum(double **arr1,int m,int n,double **sm){
 			sum[i]+=arr1[i][j];
 		}
 	}
-	sm=&sum;
+	//sm=&sum;
 }
 
-void transpose(double **arr1,int m,int n,double ***trans){
+void transpose(double **arr1,int m,int n,double **trans){
 	int i,j;
-	double **arr2=new double*[n];
-	for(i=0;i<m;i++){
-		arr2[i]=new double[m];
-	}
 	for(i=0;i<n;i++){
 		for(j=0;j<m;j++){
-			arr2[i][j]=arr1[j][i];
+			trans[i][j]=arr1[j][i];
 		}
 	}
-	trans=&arr2;
 }
 
-void softmax(double** arr,int m1,int n1,double ***sftm){
+void softmax(double** arr,int m1,int n1,double **soft){
 	int sum,i,j;
-	double **soft=new double*[m1];
-	for(i=0;i<m1;i++){
-		soft[i]=new double[n1];
-	}
 
 	for(i=0;i<m1;i++){
 		sum=0;
@@ -79,25 +65,19 @@ void softmax(double** arr,int m1,int n1,double ***sftm){
 			soft[i][j]=exp(arr[i][j])/sum;
 		}
 	}
-	sftm=&soft;
 }
 
-void mat_element_wise_multiply(double** a1,double** a2,int m1,int n1,int m2,int n2,double*** elematmul){
+void mat_element_wise_multiply(double** a1,double** a2,int m1,int n1,int m2,int n2,double** mul){
 	if(m1!=m2||n1!=n2){
 		cout<<"matrix dimension mismatch for element wise matrix multiplication"<<endl;
 		return ;
 	}
 	int i,j;
-	double **mul=new double*[m1];
-	for(i=0;i<m1;i++){
-		mul[i]=new double[n1];
-	}
 	for(i = 0;i<m1;i++){
         for(j = 0;j<n1;j++){
         	mul[i][j]=a1[i][j]*a2[i][j];
     	}    
 	}
-	elematmul=&mul;
 }
 double array_sum(double *arr,int n){
 	int i;
@@ -119,51 +99,41 @@ double mat_sum(double **arr,int m,int n){
 }
 
 
-void mat_addition(double** a1,double* a2,int m1,int n1,int m2,double*** matadd){
-	if(m1!=m2){
+void mat_addition(double** a1,double* a2,int m1,int n1,int m2,double** matadd){
+	if(n1!=m2){
 		cout<<"matrix dimension mismatch for matrix addition"<<endl;
 		return;
 	}
 	int i,j;
-	double **add=new double*[m1];
-	for(i=0;i<m1;i++){
-		add[i]=new double[n1];
-	}
 	for(i=0;i<m1;i++){
 		for(j=0;j<n1;j++){
-			add[i][j]=a1[i][j]+a2[j];
+			matadd[i][j]=a1[i][j]+a2[j];
 		}
 	}
-	matadd=&add;
+	// matadd=&add;
 }
 
-void mat_multiply(double** a1,double** a2,int m1,int n1,int m2,int n2,double*** matmul){
+void mat_multiply(double** a1,double** a2,int m1,int n1,int m2,int n2,double** matmul){
 	if(n1!=m2){
 		cout<<"matrix dimension mismatch for matrix multiplication"<<endl;
 		return;
 	}
 	int i,j,k;
-	double **mul=new double*[m1];
-	for(i=0;i<m1;i++){
-		mul[i]=new double[n2];
-	}
 	for(i = 0;i<m1;i++){
         for(j = 0;j<n2;j++){
+        	matmul[i][j] = 0;
             for(k = 0; k < n1;k++){
-                mul[i][j] += a1[i][k] * a2[k][j];
+                matmul[i][j] += a1[i][k] * a2[k][j];
             }
     	}    
 	}
-	print_mat(mul,m1,n2);
-	matmul=&mul;
+	printf("In matmul\n");
+	print_mat(matmul,m1,n2);
+	// matmul=&mul;
 }
 
-void relu(double** a1,int m1,int n1,double*** relfn){
+void relu(double** a1,int m1,int n1,double** act){
 	int i,j,k;
-	double **act=new double*[m1];
-	for(i=0;i<m1;i++){
-		act[i]=new double[n1];
-	}
 	for(i = 0;i<m1;i++){
         for(j = 0;j<n1;j++){
             if(a1[i][j]<0){
@@ -174,7 +144,6 @@ void relu(double** a1,int m1,int n1,double*** relfn){
             }
     	}    
 	}
-	relfn=&act;
 }
 
 int input_size = 4;
@@ -251,7 +220,7 @@ class TwoLayerNet{
 		X=&arr;
 		y=&temp;
 	}
-	double loss(double reg,double ***grads_W1,double ***grads_W2,double **grads_b1,double **grads_b2,double **X,int output_size,int *y,int hidden_size){
+	double loss(double reg,double **grads_W1,double **grads_W2,double *grads_b1,double *grads_b2,double **X,int output_size,int *y,int hidden_size){
 		printf("At start of loss function\n");
 		int i,j;
 	    //z1 = X.dot(W1) + b1
@@ -260,10 +229,11 @@ class TwoLayerNet{
 		{
 			mul[i]=(double*)malloc(hidden_size*sizeof(double));
 		}
-		mat_multiply(X,W1,num_inputs,input_size,input_size,hidden_size,&mul);
+		mat_multiply(X,W1,num_inputs,input_size,input_size,hidden_size,mul);
 		printf("mat_mul 1 done:::\n");
 		print_mat(X,num_inputs,input_size);
 		print_mat(W1,input_size,hidden_size);
+		printf("matmul2::");
 		print_mat(mul,num_inputs,hidden_size);
 
 		double **z1=(double**)malloc(num_inputs*sizeof(double*));
@@ -272,7 +242,7 @@ class TwoLayerNet{
 			z1[i]=(double*)malloc(hidden_size*sizeof(double));
 		}
 
-	    mat_addition(mul,b2,num_inputs,hidden_size,hidden_size,&z1);
+	    mat_addition(mul,b1,num_inputs,hidden_size,hidden_size,z1);
 	    printf("mat_add 2 done:::\n");
 		print_mat(z1,num_inputs,hidden_size);
 
@@ -283,7 +253,7 @@ class TwoLayerNet{
 			a1[i]=(double*)malloc(hidden_size*sizeof(double));
 		}
 
-	    relu(z1,num_inputs,hidden_size,&a1);
+	    relu(z1,num_inputs,hidden_size,a1);
 	    printf("relu 3 done:::\n");
 		print_mat(a1,num_inputs,hidden_size);
 		
@@ -293,8 +263,7 @@ class TwoLayerNet{
 		{
 			mul2[i]=(double*)malloc(output_size*sizeof(double));
 		}
-		mat_multiply(X,W1,num_inputs,hidden_size,hidden_size,output_size,&mul2);
-
+		mat_multiply(a1,W2,num_inputs,hidden_size,hidden_size,output_size,mul2);
 		printf("mat_mul 4 done:::\n");
 		print_mat(mul2,num_inputs,output_size);
 
@@ -304,7 +273,7 @@ class TwoLayerNet{
 			score[i]=(double*)malloc(output_size*sizeof(double));
 		}
 
-	    mat_addition(mul2,b2,num_inputs,output_size,output_size,&score);
+	    mat_addition(mul2,b2,num_inputs,output_size,output_size,score);
 
 
 		printf("mat_add 5 done:::\n");
@@ -316,7 +285,7 @@ class TwoLayerNet{
 			probs[i]=(double*)malloc(output_size*sizeof(double));
 		}
 
-	    softmax(score,num_inputs,output_size,&probs);
+	    softmax(score,num_inputs,output_size,probs);
 
 
 		printf("softmax 6 done:::\n");
@@ -345,8 +314,8 @@ class TwoLayerNet{
 		{
 			elemul2[i]=(double*)malloc(output_size*sizeof(double));
 		}
-		mat_element_wise_multiply(W1,W1,input_size,hidden_size,input_size,hidden_size,&elemul1);
-		mat_element_wise_multiply(W2,W2,hidden_size,output_size,hidden_size,output_size,&elemul2);
+		mat_element_wise_multiply(W1,W1,input_size,hidden_size,input_size,hidden_size,elemul1);
+		mat_element_wise_multiply(W2,W2,hidden_size,output_size,hidden_size,output_size,elemul2);
 
 
 		printf("mat_ele_mul 7 done:::\n");
@@ -364,13 +333,12 @@ class TwoLayerNet{
 	    //dscores = probs
 	    //dscores[range(N),y] -= 1
 	    //dscores /= N
-
 	    double **dscores=(double**)malloc(num_inputs*sizeof(double*));
 		for(int i=0;i<num_inputs;i++)
 		{
 			dscores[i]=(double*)malloc(output_size*sizeof(double));
 		}
-	    copy_matrix(probs,num_inputs,output_size,&dscores);
+	    copy_matrix(probs,num_inputs,output_size,dscores);
 	    for(i=0;i<num_inputs;i++){
 	    	dscores[i][y[i]]-=1;
 	    }
@@ -379,7 +347,6 @@ class TwoLayerNet{
 	    		dscores[i][j]/=num_inputs;
 	    	}
 	    }
-	    
 	    //grads['W2'] = np.dot(a1.T, dscores)
 	    //grads['b2'] = np.sum(dscores, axis=0) i.e. row wise sum
 	    double **transa1=(double**)malloc(hidden_size*sizeof(double*));
@@ -387,32 +354,37 @@ class TwoLayerNet{
 		{
 			transa1[i]=(double*)malloc(num_inputs*sizeof(double));
 		}
-		double **mul3=(double**)malloc(hidden_size*sizeof(double*));
+		/*double **mul3=(double**)malloc(hidden_size*sizeof(double*));
 		for(int i=0;i<hidden_size;i++)
 		{
 			mul3[i]=(double*)malloc(output_size*sizeof(double));
-		}
-		transpose(a1,num_inputs,hidden_size,&transa1);
-		mat_multiply(transa1,dscores,hidden_size,num_inputs,num_inputs,output_size,&mul3);
-		grads_W2=&mul3;
+		}*/
+		transpose(a1,num_inputs,hidden_size,transa1);
+		mat_multiply(transa1,dscores,hidden_size,num_inputs,num_inputs,output_size,grads_W2);
+		//grads_W2=&mul3;
 	    
-	    double *rowsum=(double*)malloc(num_inputs*sizeof(double*));
-	    mat_row_wise_sum(dscores,num_inputs,output_size,&rowsum);
-	    grads_b2=&rowsum;
-
+	    //double *rowsum=(double*)malloc(num_inputs*sizeof(double*));
+	    mat_row_wise_sum(dscores,num_inputs,output_size,grads_b2);
+	    printf("mat_rw wise sum 9 done:::\n");
+		print_mat(&grads_b2,1,num_inputs);
+		printf("Reached here1\n");
+	    //grads_b2=&rowsum;
 	    //dhidden = np.dot(dscores, W2.T)
 	    double **transW2=(double**)malloc(output_size*sizeof(double*));
 		for(int i=0;i<output_size;i++)
 		{
-			transa1[i]=(double*)malloc(hidden_size*sizeof(double));
+			transW2[i]=(double*)malloc(hidden_size*sizeof(double));
 		}
+		printf("Reached here1\n");
 		double **dhidden=(double**)malloc(num_inputs*sizeof(double*));
 		for(int i=0;i<num_inputs;i++)
 		{
 			dhidden[i]=(double*)malloc(hidden_size*sizeof(double));
 		}
-
-	    mat_multiply(dscores,transW2,num_inputs,output_size,output_size,hidden_size,&dhidden);
+		printf("Reached here1\n");
+		transpose(W2,hidden_size,output_size,transW2);
+	    mat_multiply(dscores,transW2,num_inputs,output_size,output_size,hidden_size,dhidden);
+	    printf("Reached here2\n");
 	    //backprop the ReLU non-linearity
 	    for(i=0;i<num_inputs;i++){
 	    	for(j=0;j<hidden_size;j++){
@@ -421,6 +393,7 @@ class TwoLayerNet{
 	    		}
 	    	}
 	    }
+
 	    //finally into W,b
 	    //grads['W1'] = np.dot(X.T, dhidden)
 	    //grads['b1'] = np.sum(dhidden, axis=0)
@@ -429,30 +402,31 @@ class TwoLayerNet{
 		{
 			transX[i]=(double*)malloc(num_inputs*sizeof(double));
 		}
-		double **mul4=(double**)malloc(num_inputs*sizeof(double*));
+		/*double **mul4=(double**)malloc(num_inputs*sizeof(double*));
 		for(int i=0;i<num_inputs;i++)
 		{
 			mul4[i]=(double*)malloc(hidden_size*sizeof(double));
-		}
-		transpose(X,num_inputs,input_size,&transX);
-		mat_multiply(transX,dhidden,num_inputs,input_size,input_size,hidden_size,&mul4);
-		grads_W1=&mul4;
-
-		double *rowsum2=(double*)malloc(num_inputs*sizeof(double*));
-		mat_row_wise_sum(dhidden,num_inputs,hidden_size,&rowsum2);
-		grads_b1=&rowsum2;
-	    
+		}*/
+		transpose(X,num_inputs,input_size,transX);
+		mat_multiply(transX,dhidden,input_size,num_inputs,num_inputs,hidden_size,grads_W1);
+		//grads_W1=&mul4;
+		 printf("Reached here3\n");
+		//double *rowsum2=(double*)malloc(num_inputs*sizeof(double*));
+		mat_row_wise_sum(dhidden,num_inputs,hidden_size,grads_b1);
+		//grads_b1=&rowsum2;
+	     printf("Reached here4\n");
 	    //add regularization gradient contribution
 	    //grads['W2'] += reg * W2
 	    //grads['W1'] += reg * W1
 	    for(i=0;i<hidden_size;i++){
 	    	for(j=0;j<output_size;j++){
-	    		*grads_W2[i][j]+=W2[i][j]*reg;
+	    		grads_W2[i][j]+=W2[i][j]*reg;
 	    	}
 	    }
-	    for(i=0;i<num_inputs;i++){
+
+	    for(i=0;i<input_size;i++){
 	    	for(j=0;j<hidden_size;j++){
-	    		*grads_W1[i][j]+=W1[i][j]*reg;
+	    		grads_W1[i][j]+=W1[i][j]*reg;
 	    	}
 	    }
 	    return loss_f;
@@ -478,7 +452,7 @@ int main(){
 			grads_W1[i]=(double*)malloc(hidden_size*sizeof(double));
 		}
 	grads_W2=(double**)malloc(hidden_size*sizeof(double*));
-		for(int i=0;i<num_inputs;i++)
+		for(int i=0;i<hidden_size;i++)
 		{
 			grads_W2[i]=(double*)malloc(output_size*sizeof(double));
 		}
@@ -499,9 +473,9 @@ int main(){
 	}
 	
 	//print_mat(X,num_inputs,input_size);//print_mat(net.W1,input_size,hidden_size);print_mat(net.W2,hidden_size,output_size);
-	double loss=net.loss(0.1,&grads_W1,&grads_W2,&grads_b1,&grads_b2,X,output_size,y,hidden_size);
-	//double correct_loss = 1.30378789133;
-	//cout<<"difference between your loss and correct loss is::"<<loss-correct_loss;
+	double loss=net.loss(0.1,grads_W1,grads_W2,grads_b1,grads_b2,X,output_size,y,hidden_size);
+	double correct_loss = 1.30378789133;
+	cout<<"difference between your loss and correct loss is::"<<loss-correct_loss;
 	
 	return 0;
 }
